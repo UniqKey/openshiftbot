60fc342563551251418a067077b26093efa4c973
simbo1905 <simbo1905@60hertz.com>
2018-05-28 21:01:35 +0100

    bin/promote.sh

--- bin/promote.sh
+++ bin/promote.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 IMAGE=$1
-TAG=$(date +"%Y-%m-%d_%H-%M-%S")
-echo promoting $IMAGE using tag $TAG
-./oc tag $PROMOTE_PROJECT_FROM/$IMAGE:latest $PROMOTE_PROJECT_FROM/$IMAGE:$TAG
-./oc tag $PROMOTE_PROJECT_FROM/$IMAGE:$TAG $PROMOTE_PROJECT_TO/$IMAGE:latest
-./oc get dc $IMAGE
+
+if [ ! -z ${IMAGE+x} ]; then 
+
+    TAG=$(date +"%Y-%m-%d_%H-%M-%S")
+    echo promoting $IMAGE using tag $TAG
+    ./oc tag $PROMOTE_PROJECT_FROM/$IMAGE:latest $PROMOTE_PROJECT_FROM/$IMAGE:$TAG
+    ./oc tag $PROMOTE_PROJECT_FROM/$IMAGE:$TAG $PROMOTE_PROJECT_TO/$IMAGE:latest
+    ./oc get dc $IMAGE -n $PROMOTE_PROJECT_TO
+
+else 
+
+    "please specify an image."
+
+fi
