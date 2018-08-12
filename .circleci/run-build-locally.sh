7bc9362aa1fbfe7ad50f80812dfc8fa64bba3a22
Simon Massey <simon@simonmassey.org>
2018-08-12 16:42:54 +0100

    removed circleci build, restored github hook on master

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=$(git rev-parse HEAD) \
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/simbo1905/openshiftbot2/tree/master
